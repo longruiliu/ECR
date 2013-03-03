710b1415ac08d98bc996f668fa0f3bc3b7cd4351
Kai Luo <gluokai@gmail.com>
2013-03-03 16:34:42 +0800

    maybe 10% of the module now

--- parser.cc
+++ parser.cc
@@ -1,9 +1,47 @@
 #include <json>
 #include "parser.h"
 
-string jsonToString(Json::value &);
+std::string jsonToString(Json::value &);
 Json::value stringToJson(std::string &);
 Request jsonToRequest(Json::value &);
 Json::Value requestToJson(Request &);
+
+std::string jsonToString(Json::value &json) {
+    Json::FastWriter writer;
+    return writer.write(json);
+}
+
+Json::Value stringToJson(std::string &str) {
+    Json::reader reader;
+    Json::Value ret;
+    if (reader.parse(str, ret))
+        // add a "ERROR" property for the result json obj
+        ret["ERROR"] = "true";
+    else
+        ret["ERROR"] = "false";
+    return ret;
+}
+
+Request jsonToRequest(Json::value &json) {
+    
+}
+
+Json::Value requestToJson(Request &req) {
+    Json::Value ret;
+    ret["sessionID"] = req.sessionID;
+    ret["op"] = req.op;
+    ret["payload"] = req.payload;
+    return ret;
+}
+
+
+void retUserInfo(map <std::string, std::string> &ui) {
+    Json::Value json;
+    for (__typeof(ui.begin()) it = ui.begin(); it != ui.end(); it++)
+        json[*it.first] = ui[*it.second];
+    std::string ret = jsonToString(json);
+    // call lower level function for delivery
+}
+
 
 
