20b42bc470a177202682cf660d0635f53f8b6915
Kai Luo <gluokai@gmail.com>
2013-03-06 15:14:49 +0800

    var json -> var root

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 protocol.o: protocol.cc
-	g++ -c protocol.cc -Iinclude -Llib/json -ljson
+	g++ -c protocol.cc -Iinclude -g
+
+protocol: protocol.o
+	g++ -Llib protocol.o -ljson -o protocol -g
+
 clean:
 	rm *.o
--- protocol.cc
+++ protocol.cc
@@ -6,23 +6,26 @@
 #include <vector>
 #include "protocol.h"
 
-int jsonToString(Json::Value &, std::string &);
+int rootToString(Json::Value &, std::string &);
 int stringToJson(std::string &, Json::Value &);
 
-int jsonToString(Json::Value &json, std::string &ret) {
+int rootToString(Json::Value &root, std::string &ret) {
     Json::FastWriter writer;
-    ret = writer.write(json);
+    ret = writer.write(root);
 }
 
-int stringToJson(std::string &str, Json::Value &json) {
+int stringToJson(std::string &str, Json::Value &root) {
     Json::Reader reader;
-    if (!reader.parse(str, json)) return ERROR;
+    if (!reader.parse(str, root))
+        return ERROR;
     return 0;
 }
 
-int requestHandler(std::string &request, std::string &response)
-{
-    Json::Value json;
-    if (stringToJson(request, json) == ERROR)
+int requestHandler(std::string &request, std::string &response) {
+    Json::Value root;
+    if (stringToJson(request, root) == ERROR)
         return INVALID_REQUEST;
+    // call logic layer module
+    return 0;
 }
+
