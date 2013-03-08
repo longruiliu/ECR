fb4c61c6772c9299be5e49a4cb57031db12f1043
Kai Luo <gluokai@gmail.com>
2013-03-08 20:39:11 +0800

    protocol module v0.2?

--- protocol.cc
+++ protocol.cc
@@ -10,6 +10,7 @@
 #include "src-srv/logic/sessionControl.h"
 #include "src-srv/logic/logic.h"
 
+typedef std::vector <std::string> Args;
 
 static int jsonToString(Json::Value &, std::string &);
 static int stringToJson(std::string &, Json::Value &);
@@ -25,6 +26,20 @@ static int stringToJson(std::string &str, Json::Value &root) {
     if (!reader.parse(str, root))
         return ERROR;
     return 0;
+}
+
+static int getLines(std::string lines, std::vector <std::string> &line) {
+    int prev = 0, len = lines.size();
+    for (int i = 0; i != len; i++) {
+        if (lines[i] == '\n') {
+            if (i-prev)
+                line.push_back(lines.substr(prev, i-prev));
+            prev = i+1;
+        }
+    }
+    if (len-1-prev)
+        line.push_back(lines.substr(prev, len-1-prev));
+    return line.size();
 }
 
 int requestHandler(std::string &request) {
@@ -60,8 +75,30 @@ int requestHandler(std::string &request) {
             sscanf(params.c_str(), "%d", &groupID);
             delGroup(srcID, groupID);
         } else if (method == "adduser") {
+            Args argv;
+            if (getLines(params, argv) != 3)
+                return ERR_INVALID_PARAMS;
+            int groupID, newMemberID;
+            sscanf(argv[0].c_str(), "%d", &groupID);
+            sscanf(argv[1].c_str(), "%d", &newMemberID);
+            std::string msg = argv[2];
+            addGroupMember(srcID, groupID, newMemberID, msg);
         } else if (method == "deluser") {
+            Args argv;
+            int groupID, memberID;
+            if (getLines(params, argv) != 2)
+                return ERR_INVALID_PARAMS;
+            sscanf(argv[0].c_str(), "%d", &groupID);
+            sscanf(argv[1].c_str(), "%d", &memberID);
+            delGroupMember(srcID, groupID, memberID);
         } else if (method == "sendmsg") {
+            Args argv;
+            if (getLines(params, argv) != 2)
+                return ERR_INVALID_PARAMS;
+            int groupID;
+            sscanf(argv[0].c_str(), "%d", &groupID);
+            std::string msg = argv[1];
+            sendGroupMsg(srcID, groupID, msg);
         } else if (method == "joinreq") {
         } else if (method =="quitreq") {
         } else if (method == "userlist") {
