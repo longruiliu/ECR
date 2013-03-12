2cd34fba4eecbf27edc784457f5691b56715d93e
Kai Luo <gluokai@gmail.com>
2013-03-12 09:48:43 +0800

    outline completed

---
+++ src-srv/prototype/requestHandlers.py
@@ -0,0 +1,19 @@
+groupHandlers = {
+    "add": addGroup,
+    "del": delGroup,
+    "adduser": addGroupMember,
+    "deluser": delGroupMember,
+    "userlist": fetchMemberList,
+    "redmsg": sendResMsg
+    }
+
+regularHandlers = {
+    "login": login,
+    "logout": logout,
+    "sendmsg": sendMsg,
+    "userinfo": getUserInfo,
+    "add": addUser,
+    "modify": modifyUser,
+    "del": delUser,
+    "fetch": fetchMsg
+    }
